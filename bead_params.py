1059f8965de6306214b915d8b1b7b1c399e9194b
catohaste <catohaste@users.noreply.github.com>
2021-01-25 00:55:58 +0000

    Add framework for new parameter variation results.
    New param variation is separate run for each experiment.

--- bead_params.py
+++ bead_params.py
@@ -118,3 +118,208 @@ bead_params_play = {
     'DM_conc' : -0.496848222171755,  # AG1X2
     'SB_conc' : -0.5 # AG1X2
 }
+
+bead_params_all_exps = {
+    
+    # vg1
+    'cell_pellet_width' : 30,
+    
+    'cell_pellet_spread' : 23.1982681897682,
+    
+    'vg1_cell_conc' : 0.59474841251661, # cell_pellet 
+    'bmp4_cell_conc' : 0.709944612721701,
+
+    # bmp
+    'afigel_width' : 7,
+    
+    'afigel_50_spread' : 40,    # unused
+    'afigel_25_spread' : 52.104054405184,
+    'afigel_12_spread' : 7.56592055048768,
+    'afigel_6_spread' : 8.12703482365632,
+    
+    'bmp4_50_conc' : 0.9,  # unused # afigel
+    'bmp4_25_conc' : 0.753881497135208,
+    'bmp4_12_conc' : 0.654662069443632, # 0.125,
+    'bmp4_6_conc' : 0.686197808523482,
+
+    # activin
+    'heparin_width' : 9,
+    
+    'heparin_10_spread' : 26.2076237282151, # 0.143, # heparin
+    'heparin_2_spread' : 37.0974682692167,
+    
+    'activin_10_conc' : 0.46292245853416, # 0.143, # heparin
+    'activin_2_conc' : 0.528709356400615,
+    
+    # DM and SB
+    'AG1X2_width' : 7,
+    
+    'AG1X2_spread' : 52.5385697426049,
+    
+    'DM_conc' : -0.496891765965946,  # AG1X2
+    'SB_conc' : -0.5 # AG1X2
+}
+
+bead_params_cell_pellet = {
+    
+    # vg1
+    'cell_pellet_width' : 30,
+    
+    'cell_pellet_spread' : 7,
+    
+    'vg1_cell_conc' : 0.390605285016128, # cell_pellet 
+    'bmp4_cell_conc' : 0.573597304673746,
+
+    # bmp
+    'afigel_width' : 7,
+    
+    'afigel_50_spread' : 40,
+    'afigel_25_spread' : 56.9324759161971,
+    'afigel_12_spread' : 6.94129297198067,
+    'afigel_6_spread' : 9.40279474053165,
+    
+    'bmp4_50_conc' : 0.9,  # afigel
+    'bmp4_25_conc' : 0.00634559907647732,
+    'bmp4_12_conc' : 0.996145149040218, # 0.125,
+    'bmp4_6_conc' : 0.928947829600234,
+
+    # activin
+    'heparin_width' : 9,
+    
+    'heparin_10_spread' : 22.2541462480205, # 0.143, # heparin
+    'heparin_2_spread' : 14.4820334029303,
+    
+    'activin_10_conc' : 0.561934865879604, # 0.143, # heparin
+    'activin_2_conc' : 0.100308721619127,
+    
+    # DM and SB
+    'AG1X2_width' : 7,
+    
+    'AG1X2_spread' : 31.4225304206746,
+    
+    'DM_conc' : -0.496848222171755,  # AG1X2
+    'SB_conc' : -0.5 # AG1X2
+}
+
+bead_params_activin_ant = {
+    
+    # vg1
+    'cell_pellet_width' : 30,
+    
+    'cell_pellet_spread' : 7,
+    
+    'vg1_cell_conc' : 0.390605285016128, # cell_pellet 
+    'bmp4_cell_conc' : 0.573597304673746,
+
+    # bmp
+    'afigel_width' : 7,
+    
+    'afigel_50_spread' : 40,
+    'afigel_25_spread' : 56.9324759161971,
+    'afigel_12_spread' : 6.94129297198067,
+    'afigel_6_spread' : 9.40279474053165,
+    
+    'bmp4_50_conc' : 0.9,  # afigel
+    'bmp4_25_conc' : 0.00634559907647732,
+    'bmp4_12_conc' : 0.996145149040218, # 0.125,
+    'bmp4_6_conc' : 0.928947829600234,
+
+    # activin
+    'heparin_width' : 9,
+    
+    'heparin_10_spread' : 22.2541462480205, # 0.143, # heparin
+    'heparin_2_spread' : 14.4820334029303,
+    
+    'activin_10_conc' : 0.561934865879604, # 0.143, # heparin
+    'activin_2_conc' : 0.100308721619127,
+    
+    # DM and SB
+    'AG1X2_width' : 7,
+    
+    'AG1X2_spread' : 31.4225304206746,
+    
+    'DM_conc' : -0.496848222171755,  # AG1X2
+    'SB_conc' : -0.5 # AG1X2
+}
+
+bead_params_bmp4_ant = {
+    
+    # vg1
+    'cell_pellet_width' : 30,
+    
+    'cell_pellet_spread' : 7,
+    
+    'vg1_cell_conc' : 0.390605285016128, # cell_pellet 
+    'bmp4_cell_conc' : 0.573597304673746,
+
+    # bmp
+    'afigel_width' : 7,
+    
+    'afigel_50_spread' : 40,
+    'afigel_25_spread' : 56.9324759161971,
+    'afigel_12_spread' : 6.94129297198067,
+    'afigel_6_spread' : 9.40279474053165,
+    
+    'bmp4_50_conc' : 0.9,  # afigel
+    'bmp4_25_conc' : 0.00634559907647732,
+    'bmp4_12_conc' : 0.996145149040218, # 0.125,
+    'bmp4_6_conc' : 0.928947829600234,
+
+    # activin
+    'heparin_width' : 9,
+    
+    'heparin_10_spread' : 22.2541462480205, # 0.143, # heparin
+    'heparin_2_spread' : 14.4820334029303,
+    
+    'activin_10_conc' : 0.561934865879604, # 0.143, # heparin
+    'activin_2_conc' : 0.100308721619127,
+    
+    # DM and SB
+    'AG1X2_width' : 7,
+    
+    'AG1X2_spread' : 31.4225304206746,
+    
+    'DM_conc' : -0.496848222171755,  # AG1X2
+    'SB_conc' : -0.5 # AG1X2
+}
+
+bead_params_threshold = {
+    
+    # vg1
+    'cell_pellet_width' : 30,
+    
+    'cell_pellet_spread' : 7,
+    
+    'vg1_cell_conc' : 0.390605285016128, # cell_pellet 
+    'bmp4_cell_conc' : 0.573597304673746,
+
+    # bmp
+    'afigel_width' : 7,
+    
+    'afigel_50_spread' : 40,
+    'afigel_25_spread' : 56.9324759161971,
+    'afigel_12_spread' : 6.94129297198067,
+    'afigel_6_spread' : 9.40279474053165,
+    
+    'bmp4_50_conc' : 0.9,  # afigel
+    'bmp4_25_conc' : 0.00634559907647732,
+    'bmp4_12_conc' : 0.996145149040218, # 0.125,
+    'bmp4_6_conc' : 0.928947829600234,
+
+    # activin
+    'heparin_width' : 9,
+    
+    'heparin_10_spread' : 22.2541462480205, # 0.143, # heparin
+    'heparin_2_spread' : 14.4820334029303,
+    
+    'activin_10_conc' : 0.561934865879604, # 0.143, # heparin
+    'activin_2_conc' : 0.100308721619127,
+    
+    # DM and SB
+    'AG1X2_width' : 7,
+    
+    'AG1X2_spread' : 31.4225304206746,
+    
+    'DM_conc' : -0.496848222171755,  # AG1X2
+    'SB_conc' : -0.5 # AG1X2
+}
--- model_params.py
+++ model_params.py
@@ -1,31 +1,36 @@
 from classes import Model
 import numpy as np
 
-from bead_params import bead_params_init, bead_params_fair, bead_params_play
+from bead_params import bead_params_init, bead_params_fair, bead_params_play, bead_params_all_exps, bead_params_cell_pellet, bead_params_activin_ant, bead_params_bmp4_ant, bead_params_threshold
 
 # define models
 modelN = 2
 models = [Model() for i in range(modelN)]
 
-models[0].name = 'inducer_SMAD'
-models[0].label = 'Without\nnbhd'
-models[0].index_string = 'A'
-models[0].plot_color = 'C8'
-models[0].bead_params = bead_params_fair
-models[0].threshold = 0.298905377484657
-models[0].nbhd_size = None
-models[0].inducer_scaling = 10.0 ** (1.07852964487069)
-models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+##### before ##############################################################################
 
-models[1].name = 'inducer_SMAD_nbhd'
-models[1].label = 'With\nnbhd'
-models[1].index_string = 'B'
-models[1].plot_color = 'C1'
-models[1].bead_params = bead_params_fair
-models[1].threshold = 0.489438862449442
-models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
-models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
-models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+# models[0].name = 'inducer_SMAD'
+# models[0].label = 'Without\nnbhd'
+# models[0].index_string = 'A'
+# models[0].plot_color = 'C8'
+# models[0].bead_params = bead_params_fair
+# models[0].threshold = 0.298905377484657
+# models[0].nbhd_size = None
+# models[0].inducer_scaling = 10.0 ** (1.07852964487069)
+# models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+#
+# models[1].name = 'inducer_SMAD_nbhd'
+# models[1].label = 'With\nnbhd'
+# models[1].index_string = 'B'
+# models[1].plot_color = 'C1'
+# models[1].bead_params = bead_params_fair
+# models[1].threshold = 0.489438862449442
+# models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
+# models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
+# models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+
+############################################################################################
+##### testing ##############################################################################
 
 # models[0].name = 'inducer_SMAD'
 # models[0].label = 'Without\nnbhd'
@@ -47,4 +52,119 @@ models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
 # models[1].inducer_scaling = 10.0 ** (0.313337594353507)
 # models[1].inhibitor_scaling = 10.0 ** 1.66978719065307
 
+#############################################################################################
+##### all exps ##############################################################################
 
+models[0].name = 'inducer_SMAD'
+models[0].label = 'Without\nnbhd'
+models[0].index_string = 'A'
+models[0].plot_color = 'C8'
+models[0].bead_params = bead_params_all_exps
+models[0].threshold = 0.232585001821855
+models[0].nbhd_size = None
+models[0].inducer_scaling = 10.0 ** (2.22201734127452)
+models[0].inhibitor_scaling = 10.0 ** (2.83382643185952)
+
+models[1].name = 'inducer_SMAD_nbhd'
+models[1].label = 'With\nnbhd'
+models[1].index_string = 'B'
+models[1].plot_color = 'C1'
+models[1].bead_params = bead_params_all_exps
+models[1].threshold = 0.44950416545415
+models[1].nbhd_size = 2*np.floor(59.433636026949) - 1
+models[1].inducer_scaling = 10.0 ** (-1.58937622237702)
+models[1].inhibitor_scaling = 10.0 ** 2.58202368084432
+
+################################################################################################
+##### cell pellet ##############################################################################
+
+# models[0].name = 'inducer_SMAD'
+# models[0].label = 'Without\nnbhd'
+# models[0].index_string = 'A'
+# models[0].plot_color = 'C8'
+# models[0].bead_params = bead_params_fair
+# models[0].threshold = 0.298905377484657
+# models[0].nbhd_size = None
+# models[0].inducer_scaling = 10.0 ** (1.07852964487069)
+# models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+#
+# models[1].name = 'inducer_SMAD_nbhd'
+# models[1].label = 'With\nnbhd'
+# models[1].index_string = 'B'
+# models[1].plot_color = 'C1'
+# models[1].bead_params = bead_params_fair
+# models[1].threshold = 0.489438862449442
+# models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
+# models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
+# models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+
+################################################################################################
+##### activin ant ##############################################################################
+
+# models[0].name = 'inducer_SMAD'
+# models[0].label = 'Without\nnbhd'
+# models[0].index_string = 'A'
+# models[0].plot_color = 'C8'
+# models[0].bead_params = bead_params_fair
+# models[0].threshold = 0.298905377484657
+# models[0].nbhd_size = None
+# models[0].inducer_scaling = 10.0 ** (1.07852964487069)
+# models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+#
+# models[1].name = 'inducer_SMAD_nbhd'
+# models[1].label = 'With\nnbhd'
+# models[1].index_string = 'B'
+# models[1].plot_color = 'C1'
+# models[1].bead_params = bead_params_fair
+# models[1].threshold = 0.489438862449442
+# models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
+# models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
+# models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+
+################################################################################################
+##### bmp4 ant #################################################################################
+
+# models[0].name = 'inducer_SMAD'
+# models[0].label = 'Without\nnbhd'
+# models[0].index_string = 'A'
+# models[0].plot_color = 'C8'
+# models[0].bead_params = bead_params_fair
+# models[0].threshold = 0.298905377484657
+# models[0].nbhd_size = None
+# models[0].inducer_scaling = 10.0 ** (1.07852964487069)
+# models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+#
+# models[1].name = 'inducer_SMAD_nbhd'
+# models[1].label = 'With\nnbhd'
+# models[1].index_string = 'B'
+# models[1].plot_color = 'C1'
+# models[1].bead_params = bead_params_fair
+# models[1].threshold = 0.489438862449442
+# models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
+# models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
+# models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+
+################################################################################################
+##### threshold ################################################################################
+
+# models[0].name = 'inducer_SMAD'
+# models[0].label = 'Without\nnbhd'
+# models[0].index_string = 'A'
+# models[0].plot_color = 'C8'
+# models[0].bead_params = bead_params_fair
+# models[0].threshold = 0.298905377484657
+# models[0].nbhd_size = None
+# models[0].inducer_scaling = 10.0 ** (1.07852964487069)
+# models[0].inhibitor_scaling = 10.0 ** (0.82009652169455)
+#
+# models[1].name = 'inducer_SMAD_nbhd'
+# models[1].label = 'With\nnbhd'
+# models[1].index_string = 'B'
+# models[1].plot_color = 'C1'
+# models[1].bead_params = bead_params_fair
+# models[1].threshold = 0.489438862449442
+# models[1].nbhd_size = 2*np.floor(52.7675295356746) - 1
+# models[1].inducer_scaling = 10.0 ** (-2.47545875046506)
+# models[1].inhibitor_scaling = 10.0 ** 1.80154516772199
+
+################################################################################################
